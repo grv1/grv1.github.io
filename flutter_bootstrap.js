4f667f3a2a804612932af2fbd3d2e2f9a40707ac
gaurav.dotiya <gaurav.dotiya@globe.com.ph>
2025-10-04 01:19:17 +0530

    init

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3894501989"
+    serviceWorkerVersion: "1141768003"
   }
 });
